ef1aa8272dad42759d1dedac8aca177536bd9df4
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-03-18 12:29:19 -0600

    Registro de usuarios sin token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,7 +4,8 @@ export const register = async (req, res) => {
   const { username, email, password } = req.body;
   try {
     const newUser = new User({ username, email, password });
-    await newUser.save();
+    const userSaved = await newUser.save();
+    res.status(201).json(userSaved);
   } catch (error) {
     return res.status(400).json({ message: "Error registering user" });
   }
